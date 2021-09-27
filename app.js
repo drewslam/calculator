48d0809ce45e7b7dc30fd5bb5072b9b88328e746
Andrew Souza <andrewesouza@gmail.com>
2021-09-26 22:52:46 -0700

    updated css

--- app.js
+++ app.js
@@ -2,9 +2,9 @@
 const numbers = document.querySelectorAll('[data-number]');
 const operators = document.querySelectorAll('[data-operation]');
 const acButton = document.querySelector('[data-ac]');
-const clearButton = document.querySelector('[data-clear]');
+const backButton = document.querySelector('[data-clear]');
 const enterButton = document.querySelector('#enter');
-const previous = document.querySelector('#previous');
+const previous = document.querySelector('p#previous');
 const current = document.querySelector('p#current');
 let currentValue = '';
 let previousValue = '';
@@ -12,6 +12,10 @@ let operation;
 let result;
 
 // Arithmatic Operations
+function enableDecimal() {
+        numbers[10].disabled = false;
+}
+
 function add(a,b) {
     a = parseFloat(a);
     b = parseFloat(b);
@@ -44,10 +48,8 @@ function workingValue() {
     if (currentValue.includes('.')) {
         numbers[10].disabled = true;
     }
-    if (currentValue.length > 9) {
-        numbers.forEach(button => {
-            button.disabled = true;
-        })
+    if (currentValue.length >= 9) {
+        numbers.forEach(button => button.disabled = true)
     }
     current.textContent += this.textContent;
     currentValue = current.textContent;
@@ -58,14 +60,17 @@ function operate() {
         previous.textContent = `${currentValue} ${this.dataset.operation}`;
         operation = this.dataset.operation;
         previousValue = currentValue
-        clearCurrent();
+        current.textContent = '';
         currentValue = '';
-        numbers[10].disabled = false;
+        enableDecimal();
     }
 }
 
 function enter() {
-    if (operation === '+' && currentValue !== '') {
+    if (previousValue === '' || previousValue === null) {
+        previousValue = '';
+        return;
+    } else if (operation === '+' && currentValue !== '') {
         result = add(previousValue,currentValue);
         previous.textContent = result;
         current.textContent = '';
@@ -81,22 +86,29 @@ function enter() {
         result = divide(previousValue,currentValue);
         previous.textContent = result;
         current.textContent = '';
+    } else if (operation === '' || operation === null) {
+        return;
     }
     previousValue = previous.textContent;
-    currentValue = previousValue;
+    currentValue = previous.textContent;
     previousValue = '';
     operation = '';
-    numbers[10].disabled = false
+    enableDecimal();
 }
 
-function clearCurrent() {
-    numbers[10].disabled = false
-    currentValue = '';
-    current.textContent = '';
+function backSpace() {
+    current.textContent = currentValue.slice(0, currentValue.length - 1);
+    currentValue = current.textContent;
+    if (!currentValue.indexOf('.')) {
+    enableDecimal()
+}
+    if (currentValue.length < 9) {
+        numbers.forEach(button => button.disabled = false)
+    }
 }
 
 function allClear() {
-    numbers[10].disabled = false;
+    numbers.forEach(button => button.disabled = false);
     currentValue = '';
     current.textContent = '';
     previousValue = '';
@@ -113,5 +125,5 @@ operators.forEach(button => {
     button.addEventListener('click', operate)
 });
 acButton.addEventListener('click', allClear);
-clearButton.addEventListener('click', clearCurrent);
+backButton.addEventListener('click', backSpace);
 enterButton.addEventListener('click', enter);
